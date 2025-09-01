5960c1742929603c45008316d91920c6a2e4ac17
Victor Louisa <victor.louisa@gmail.com>
2025-09-01 10:00:37 +0200

    project creation

---
+++ src/main/java/dev/louisa/jam/hub/JamHubServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.louisa.jam.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamHubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamHubServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/louisa/jam/hub/JamHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.louisa.jam.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamHubServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
