b414e07a6014999845921e2af3aa760b314ea795
Victor Louisa <victor.louisa@gmail.com>
2025-09-04 09:12:06 +0200

    add tests for Id implementations

--- src/main/java/dev/louisa/jam/hub/domain/band/BandId.java
+++ src/main/java/dev/louisa/jam/hub/domain/band/BandId.java
@@ -4,17 +4,17 @@ import dev.louisa.jam.hub.domain.band.exceptions.BandDomainException;
 import dev.louisa.jam.hub.domain.shared.Id;
 import lombok.Builder;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import static dev.louisa.jam.hub.domain.band.exceptions.BandDomainError.BAND_ID_CANNOT_BE_EMPTY;
+import static dev.louisa.jam.hub.domain.shared.Validator.validate;
 
 @Builder
 public record BandId(UUID id) implements Id {
 
     public BandId {
-        id = Optional.of(id)
-                .orElseThrow(() -> new BandDomainException(BAND_ID_CANNOT_BE_EMPTY));
+        validate(id)
+                .ifNullThrow(new BandDomainException(BAND_ID_CANNOT_BE_EMPTY));
     }
 
     public static BandId generate() {
@@ -22,12 +22,18 @@ public record BandId(UUID id) implements Id {
     }
 
     public static BandId fromUUID(UUID uuid) {
+        validate(uuid)
+                .ifNullThrow(new BandDomainException(BAND_ID_CANNOT_BE_EMPTY));
+        
         return BandId.builder()
                 .id(uuid)
                 .build();
     }
 
     public static BandId fromString(String value) {
+        validate(value)
+                .ifNullOrEmptyThrow(new BandDomainException(BAND_ID_CANNOT_BE_EMPTY));
+        
         return BandId.fromUUID(UUID.fromString(value));
     }
 }
