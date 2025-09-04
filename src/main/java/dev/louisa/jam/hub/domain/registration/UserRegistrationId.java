567e2a591647b8cf46cf65b656276d3d12cb96e0
Victor Louisa <victor.louisa@gmail.com>
2025-09-04 16:36:16 +0200

    refactor Id tests

--- src/main/java/dev/louisa/jam/hub/domain/registration/UserRegistrationId.java
+++ src/main/java/dev/louisa/jam/hub/domain/registration/UserRegistrationId.java
@@ -4,18 +4,18 @@ import dev.louisa.jam.hub.domain.registration.exceptions.UserRegistrationDomainE
 import dev.louisa.jam.hub.domain.shared.Id;
 import lombok.Builder;
 
-import java.util.Optional;
 import java.util.UUID;
 
-import static dev.louisa.jam.hub.domain.user.exceptions.UserDomainError.USER_ID_CANNOT_BE_EMPTY;
+import static dev.louisa.jam.hub.domain.registration.exceptions.UserRegistrationDomainError.USER_REGISTRATION_ID_CANNOT_BE_EMPTY;
+import static dev.louisa.jam.hub.domain.shared.Validator.validate;
 
 
 @Builder
 public record UserRegistrationId(UUID id) implements Id {
 
     public UserRegistrationId {
-        id = Optional.of(id)
-                .orElseThrow(() -> new UserRegistrationDomainException(USER_ID_CANNOT_BE_EMPTY));
+        validate(id)
+                .ifNullThrow(new UserRegistrationDomainException(USER_REGISTRATION_ID_CANNOT_BE_EMPTY));
     }
 
     public static UserRegistrationId generate() {
@@ -23,12 +23,18 @@ public record UserRegistrationId(UUID id) implements Id {
     }
 
     public static UserRegistrationId fromUUID(UUID uuid) {
+        validate(uuid)
+                .ifNullThrow(new UserRegistrationDomainException(USER_REGISTRATION_ID_CANNOT_BE_EMPTY));
+
         return UserRegistrationId.builder()
                 .id(uuid)
                 .build();
     }
 
     public static UserRegistrationId fromString(String value) {
+        validate(value)
+                .ifNullOrEmptyThrow(new UserRegistrationDomainException(USER_REGISTRATION_ID_CANNOT_BE_EMPTY));
+
         return UserRegistrationId.fromUUID(UUID.fromString(value));
     }
 }
--- src/test/java/dev/louisa/jam/hub/domain/user/UserIdTest.java
+++ src/test/java/dev/louisa/jam/hub/domain/user/UserIdTest.java
@@ -1,91 +1,38 @@
 package dev.louisa.jam.hub.domain.user;
 
-import dev.louisa.jam.hub.domain.BaseDomainTest;
+import dev.louisa.jam.hub.domain.IdTest;
 import dev.louisa.jam.hub.domain.user.exceptions.UserDomainException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
+import dev.louisa.jam.hub.exceptions.JamHubException;
 
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static dev.louisa.jam.hub.domain.user.exceptions.UserDomainError.*;
-import static org.assertj.core.api.Assertions.*;
 
-class UserIdTest extends BaseDomainTest {
-    @Test
-    void shouldCreateValidId() {
-        UserId id = UserId.builder()
-                .id(UUID.randomUUID())
-                .build();
-        assertThat(id.id()).isNotNull();
+class UserIdTest extends IdTest<UserId> {
+    @Override
+    protected Supplier<UserId> generator() {
+        return UserId::generate;
     }
 
-    @Test
-    void shouldGenerateValidId() {
-        UserId id = UserId.generate();
-        assertThat(id.id()).isNotNull();
+    @Override
+    protected Function<UUID, UserId> fromUuid() {
+        return UserId::fromUUID;
     }
 
-    @Test
-    void shouldCreateValidIdFromUuid() {
-        UserId id = UserId.fromUUID(UUID.randomUUID());
-        assertThat(id.id()).isNotNull();
+    @Override
+    protected Function<String, UserId> fromString() {
+        return UserId::fromString;
     }
 
-    @Test
-    void shouldCreateValidIdFromString() {
-        UserId id = UserId.fromString(UUID.randomUUID().toString());
-        assertThat(id.id()).isNotNull();
+    @Override
+    protected Class<? extends JamHubException> getEmptyValueExceptionClass() {
+        return UserDomainException.class;
     }
 
-    @Test
-    void shouldConvertUuidToString() {
-        final UUID uuid = UUID.randomUUID();
-        UserId id = UserId.fromUUID(uuid);
-
-        assertThat(id.toValue()).isEqualTo(uuid.toString());
-    }
-
-
-    @Test
-    void shouldBeEqualForSameUuid() {
-        UUID uuid = UUID.randomUUID();
-        UserId id1 = UserId.fromUUID(uuid);
-        UserId id2 = UserId.fromUUID(uuid);
-
-        assertThat(id1).isEqualTo(id2);
-        assertThat(id1.hashCode()).isEqualTo(id2.hashCode());
-    }
-
-    @Test
-    void shouldNotBeEqualForDifferentUuids() {
-        UserId id1 = UserId.fromUUID(UUID.randomUUID());
-        UserId id2 = UserId.fromUUID(UUID.randomUUID());
-
-        assertThat(id1).isNotEqualTo(id2);
-    }
-
-    @ParameterizedTest
-    @NullSource
-    void shouldThrowForNullUuidWhenParsingFromUuid(UUID uuid) {
-        assertThatCode(() -> UserId.fromUUID(uuid))
-                .isInstanceOf(UserDomainException.class)
-                .hasMessageContaining(USER_ID_CANNOT_BE_EMPTY.getMessage());
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void shouldThrowForEmptyValueWhenParsingFromString(String value) {
-        assertThatCode(() -> UserId.fromString(value))
-                .isInstanceOf(UserDomainException.class)
-                .hasMessageContaining(USER_ID_CANNOT_BE_EMPTY.getMessage());
-    }
-
-    @ParameterizedTest
-    @NullSource
-    void shouldThrowForNullUuid(UUID uuid) {
-        assertThatThrownBy(() -> UserId.builder().id(uuid).build())
-                .isInstanceOf(UserDomainException.class);
+    @Override
+    protected String getEmptyValueMessage() {
+        return USER_ID_CANNOT_BE_EMPTY.getMessage();
     }
 }
