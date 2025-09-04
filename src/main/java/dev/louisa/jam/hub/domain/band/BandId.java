3a5743912884e006f9d75eb15ee4916781118f22
Victor Louisa <victor.louisa@gmail.com>
2025-09-04 21:46:56 +0200

    replace validator with guard

--- src/main/java/dev/louisa/jam/hub/domain/band/BandId.java
+++ src/main/java/dev/louisa/jam/hub/domain/band/BandId.java
@@ -1,21 +1,21 @@
 package dev.louisa.jam.hub.domain.band;
 
 import dev.louisa.jam.hub.domain.band.exceptions.BandDomainException;
+import dev.louisa.jam.hub.domain.shared.Guard;
 import dev.louisa.jam.hub.domain.shared.Id;
 import lombok.Builder;
 
 import java.util.UUID;
 
 import static dev.louisa.jam.hub.domain.band.exceptions.BandDomainError.BAND_ID_CANNOT_BE_EMPTY;
-import static dev.louisa.jam.hub.domain.shared.Validator.validate;
 
 @Builder
 public record BandId(UUID id) implements Id {
     private static final BandDomainException EMPTY_ID_EXCEPTION = new BandDomainException(BAND_ID_CANNOT_BE_EMPTY);
 
     public BandId {
-        validate(id)
-                .ifNullThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(id == null)
+                .thenThrow(EMPTY_ID_EXCEPTION);
     }
 
     public static BandId generate() {
@@ -23,8 +23,8 @@ public record BandId(UUID id) implements Id {
     }
 
     public static BandId fromUUID(UUID uuid) {
-        validate(uuid)
-                .ifNullThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(uuid == null)
+                .thenThrow(EMPTY_ID_EXCEPTION);
         
         return BandId.builder()
                 .id(uuid)
@@ -32,8 +32,8 @@ public record BandId(UUID id) implements Id {
     }
 
     public static BandId fromString(String value) {
-        validate(value)
-                .ifNullOrEmptyThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(value == null || value.isBlank())
+                .thenThrow(EMPTY_ID_EXCEPTION);
         
         return BandId.fromUUID(UUID.fromString(value));
     }
--- src/main/java/dev/louisa/jam/hub/domain/registration/UserRegistrationId.java
+++ src/main/java/dev/louisa/jam/hub/domain/registration/UserRegistrationId.java
@@ -1,13 +1,13 @@
 package dev.louisa.jam.hub.domain.registration;
 
 import dev.louisa.jam.hub.domain.registration.exceptions.UserRegistrationDomainException;
+import dev.louisa.jam.hub.domain.shared.Guard;
 import dev.louisa.jam.hub.domain.shared.Id;
 import lombok.Builder;
 
 import java.util.UUID;
 
 import static dev.louisa.jam.hub.domain.registration.exceptions.UserRegistrationDomainError.USER_REGISTRATION_ID_CANNOT_BE_EMPTY;
-import static dev.louisa.jam.hub.domain.shared.Validator.validate;
 
 
 @Builder
@@ -15,8 +15,8 @@ public record UserRegistrationId(UUID id) implements Id {
     private static final UserRegistrationDomainException EMPTY_ID_EXCEPTION = new UserRegistrationDomainException(USER_REGISTRATION_ID_CANNOT_BE_EMPTY);
 
     public UserRegistrationId {
-        validate(id)
-                .ifNullThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(id == null)
+                .thenThrow(EMPTY_ID_EXCEPTION);
     }
 
     public static UserRegistrationId generate() {
@@ -24,8 +24,8 @@ public record UserRegistrationId(UUID id) implements Id {
     }
 
     public static UserRegistrationId fromUUID(UUID uuid) {
-        validate(uuid)
-                .ifNullThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(uuid == null)
+                .thenThrow(EMPTY_ID_EXCEPTION);
 
         return UserRegistrationId.builder()
                 .id(uuid)
@@ -33,8 +33,8 @@ public record UserRegistrationId(UUID id) implements Id {
     }
 
     public static UserRegistrationId fromString(String value) {
-        validate(value)
-                .ifNullOrEmptyThrow(EMPTY_ID_EXCEPTION);
+        Guard.when(value == null || value.isBlank())
+                .thenThrow(EMPTY_ID_EXCEPTION);
 
         return UserRegistrationId.fromUUID(UUID.fromString(value));
     }
