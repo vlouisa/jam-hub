43a57ebb0814ce04be09e2329c4c77daf287ffb8
Victor Louisa <victor.louisa@gmail.com>
2025-09-11 18:40:12 +0200

    clean-up

---
+++ src/main/java/dev/louisa/jam/hub/infrastructure/security/util/SecurityLevelConfig.java
@@ -0,0 +1,20 @@
+package dev.louisa.jam.hub.infrastructure.security.util;
+
+import dev.louisa.jam.hub.infrastructure.security.UnsecuredEndpoints;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+public class SecurityLevelConfig {
+
+    @Bean
+    public UnsecuredEndpoints unsecuredEndpoints() {
+        return new UnsecuredEndpoints(List.of(
+                Map.entry("/api/v1/registrations", "POST"),
+                Map.entry("/api/v1/registrations/{otp}/verify", "POST")
+        ));
+    }
+}
